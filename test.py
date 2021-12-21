3aeeb439154a274b9612d64d1b10de53d5a76337
zhenguo_kong <zhenguo_kong@126.com>
2021-12-21 16:53:09 +0800

    mock

---
+++ test.py
@@ -0,0 +1,35 @@
+list1 = [1, [2, 3, 4], 5, 6, [7, [8, 9]]]
+
+
+def flatten(list2):
+    if type(list2) == list:
+        for t in range(len(list2)):
+            for e in flatten(list2[t]):
+                yield e
+    else:
+        yield list2
+
+
+for t in flatten(list1):
+    print(t)
+
+
+# encoding:UTF-8
+def yield_test(n):
+    for i in range(n):
+        yield call(i)
+        print("i=", i)
+        # 做一些其它的事情
+    print("do something.")
+    print("end.")
+
+
+def call(i):
+    return i * 2
+
+
+# 使用for循环
+for i in yield_test(5):
+    print(i, ",")
+var = lambda a, b, c: print(a, b, c)
+var(1, 2, 3)
